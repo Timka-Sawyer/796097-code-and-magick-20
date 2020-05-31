d17995b51580b0aa108547ecbe603f72a12fcb34
Timka-Sawyer <59228217+Timka-Sawyer@users.noreply.github.com>
2020-05-31 23:26:37 +0500

    выполнил задание

--- js/stat.js
+++ js/stat.js
@@ -6,18 +6,17 @@ var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var BAR_GAP = 50;
 var FONT_GAP = 10;
-var TEXT_WIDTH = 10;
 var BAR_WIDTH = 40;
 var BAR_Y = 90;
 var NAMES_Y = 260;
 var BAR_HEIGHT = 150;
 
-var renderCloud = function(ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getMaxElement = function(arr) {
+var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
   for (var i = 0; i < arr.length; i++) {
@@ -29,19 +28,19 @@ var getMaxElement = function(arr) {
   return maxElement;
 };
 
-window.renderStatistics = function(ctx, players, times) {
+window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X + 10, CLOUD_Y + 10, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#ffffff');
 
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
-  ctx.fillText("Ура вы победили!", 130, 40);
-  ctx.fillText("Список результатов:", 130, 60);
+  ctx.fillText('Ура вы победили!', 130, 40);
+  ctx.fillText('Список результатов:', 130, 60);
 
   var maxTime = getMaxElement(times);
 
-  for(var i = 0; i < players.length; i++) {
-    if(players[i] == 'Вы') {
+  for (var i = 0; i < players.length; i++) {
+    if (players[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
       ctx.fillStyle = 'hsl(250, ' + Math.round(255 * Math.random()) + '% , 50%)';
